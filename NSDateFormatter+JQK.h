b8673cc8356df1603b3393959d18b392a1f119af
Xwoder <Xwoder@vip.qq.com>
2017-10-31 11:24:36 +0800

    添加 .gitignore

---
+++ NSDateFormatter+JQK.h
@@ -0,0 +1,17 @@
+//
+//  NSDateFormatter+JQK.h
+//  NSDateFormatter+JQK
+//
+//  Created by Xwoder on 2017/10/31.
+//  Copyright © 2017年 Xwoder. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSDateFormatter (JQK)
+
++ (instancetype)formatterForRFC1123;
+
++ (instancetype)formatterForRFC850;
+
+@end
---
+++ NSDateFormatter+JQK.m
@@ -0,0 +1,27 @@
+//
+//  NSDateFormatter+JQK.m
+//  NSDateFormatter+JQK
+//
+//  Created by Xwoder on 2017/10/31.
+//  Copyright © 2017年 Xwoder. All rights reserved.
+//
+
+#import "NSDateFormatter+JQK.h"
+
+@implementation NSDateFormatter (JQK)
+
++ (instancetype)formatterForRFC1123 {
+    NSDateFormatter *dateFormatter = [NSDateFormatter new];
+    dateFormatter.timeZone = [NSTimeZone timeZoneWithAbbreviation:@"GMT"];
+    dateFormatter.dateFormat = @"EEE, dd MMM yyyy HH:mm:ss z";
+    return dateFormatter;
+}
+
++ (instancetype)formatterForRFC850 {
+    NSDateFormatter *dateFormatter = [NSDateFormatter new];
+    dateFormatter.timeZone = [NSTimeZone timeZoneWithAbbreviation:@"GMT"];
+    dateFormatter.dateFormat = @"EEE, dd-MMM-yy HH:mm:ss z";
+    return dateFormatter;
+}
+
+@end
